ebaf4787414adde395e629bd2cfb684cf6c957dd
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-02-12 15:46:19 +0000

    I couldn't Finish
    It is enough for today, A little bit frustrated because I was staring the computers and anything I coudn't get done, I will try better next time

---
+++ Strives/FullStack/2021/Feb21/M1/D10/index.js
@@ -0,0 +1,15 @@
+        let points = 0
+        let restarThePoints = points - 1  
+        //let buttons = document.getElementsByClassName("theAnswer") 
+        let myArray =[]
+
+        function answering(event){
+            let result = event.target.style.backgroundColor ='red'
+            let result2 = result + 1
+           return result2
+    }         
+    for (let i = 0; i < 11; i++) {
+        saving[i] = [];
+    //Sorry I couldn't finishing I was here all day triying to save the numbers in array, and rest that number with the wrongs number I just tired but 
+    //i will do better, i promise :)
+}
